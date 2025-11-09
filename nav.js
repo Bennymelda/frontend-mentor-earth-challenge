1a104064fc6a88eb710c56cbf0b526fc37a102bc
Benny Melda <bennymelda@gmail.com>
2025-11-09 01:42:21 +0100

    Redeploy latest changes

--- nav.js
+++ nav.js
@@ -16,6 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
   // Highlight current page
+  /*
   highlightActiveLink();
 
   function highlightActiveLink() {
@@ -34,6 +35,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   }
 
+  // Close menu on mobile when a link is clicked
+  links.forEach(link => {
+    link.addEventListener('click', () => {
+      if (menu.classList.contains('show')) {
+        menu.classList.remove('show');
+        burger.src = 'image/icon-hamburger.svg';
+        menuOpen = false;
+      }
+    });
+  });
+});
+*/
+  // Highlight current page
+  highlightActiveLink();
+
+  function highlightActiveLink() {
+    let currentPage = window.location.pathname.split('/').pop();
+    if (!currentPage || currentPage === '/' || currentPage === '') {
+      currentPage = 'index.html';
+    }
+
+    links.forEach(link => {
+      const linkPage = link.getAttribute('href').split('/').pop();
+      if (linkPage === currentPage) {
+        link.classList.add('active');
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  }
   // Close menu on mobile when a link is clicked
   links.forEach(link => {
     link.addEventListener('click', () => {
